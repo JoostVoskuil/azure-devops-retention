a84b52e4fced49127def206838d7125517685463
Joost Voskuil <joost@foxhole.nl>
2022-07-23 13:15:46 +0200

    package update

--- task/index.ts
+++ task/index.ts
@@ -31,7 +31,7 @@ async function run() {
             if (numberOfMonthsToRetain === 0) daysValid = calculateDaysForever();
             else daysValid = calculateDaysValid(numberOfMonthsToRetain);
 
-            const owner = `Pipeline:'Retention Task'`;
+            const owner = `Pipeline Retention Task`;
             await setBuildRetentionLease(teamProject, buildId, definitionId, daysValid, owner, connection);
             break;
          }
@@ -51,8 +51,8 @@ async function run() {
       }
       tl.setResult(tl.TaskResult.Succeeded, '');
    }
-   catch (err) {
-      tl.setResult(tl.TaskResult.Failed, err)
+   catch (err: any) {
+      tl.setResult(tl.TaskResult.Failed, err.message)
    }
 }
 
